a322fc03f35dbeca7d004b5789163532919d83d5
SmitK <smita.d.kadam02@gmail.com>
2021-12-23 17:14:44 +0530

    updated product feature added method1

--- src/main/java/com/demo/openCart/pages/ProductFeature.java
+++ src/main/java/com/demo/openCart/pages/ProductFeature.java
@@ -1,5 +1,24 @@
 package com.demo.openCart.pages;
 
+import org.openqa.selenium.WebDriver;
+
+import com.demo.openCart.utils.ElementUtil;
+
 public class ProductFeature {
+
+	public WebDriver driver;
+	public ElementUtil elementUtils;
+
+//	2. Page Constructor
+	/**
+	 * Page Constructor accepts WebDriver and initializes ElementUtil using driver.
+	*/
+	public ProductFeature(WebDriver driver) {
+		this.driver = driver;
+		elementUtils = new ElementUtil(driver);
+	}
+	
+	public void method1()
+	{}
 
 }
